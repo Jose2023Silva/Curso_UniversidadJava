e7b1dc3d04feede6a5d5b3eec1fc846170584349
Tecnico Jose Silva Cruz <144865965+Jose2023Silva@users.noreply.github.com>
2023-11-28 16:04:53 -0600

    feat: Seccion 7 Bucles
    Se ven todos los tipos de bucles en java, for, while, do-while
    ademas de usar el break, continue y las etiquetas para esas palabras
    reservadas.

---
+++ src/main/java/org/universidad/ciclos/EtiquetasLoops.java
@@ -0,0 +1,30 @@
+package org.universidad.ciclos;
+
+/**
+ *
+ * @author Jose Angel Silva
+ */
+public class EtiquetasLoops {
+    public static void main(String[] args) {
+        /*
+        Uso de etiquetas en java
+        
+        */
+        var val = 18;
+        var prod = 0;
+        // se usa la etiquea para referencia de un bloque
+        busquedaResultado:
+        for (int i = 1; i < 10; i++) {
+            System.out.println("for 1");
+            System.out.println(i);
+            for (int j = 1; j < 10; j++) {
+                System.out.println("for 2");
+                prod = i * j;
+                if(val == prod)
+                    break busquedaResultado;
+            }
+        }
+        
+        System.out.println("Se encontro el valor");
+    }
+}
